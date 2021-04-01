83bd798ec89b846fe552d3774b4264b9ad2e262a
Documenter.jl <documenter@juliadocs.github.io>
2021-04-01 01:15:01 +0000

    build based on b87d92c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PredictMDFuture","category":"page"},{"location":"#PredictMDFuture","page":"Home","title":"PredictMDFuture","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PredictMDFuture.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PredictMDFuture]","category":"page"}]
+}
